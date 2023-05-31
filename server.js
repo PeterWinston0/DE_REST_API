207b5f5c0b1b86aec413af28dc4c243b07b9f8a0
PeterWinston0 <itspeterwinston@gmail.com>
2023-06-01 00:35:45 +0200

    change server file

--- server.js
+++ server.js
@@ -60,10 +60,3 @@ app.listen(PORT ,function() {
 })
 
 module.exports = app;
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//     console.error(err.message);
-//     if (!err.statusCode) err.statusCode = 500;
-//     res.status(err.statusCode).send(err.message)
-// })
